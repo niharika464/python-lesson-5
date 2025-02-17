6ef9974690b39e2092336d62e4a753731cada9f0
niharika464 <niharikasinghgaur11@gmail.com>
2025-02-17 21:55:09 +0530

    calculator

---
+++ cal.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def div(x,y):
+    return x/y
+def mul(x,y):
+    return x*y
+
+num1=int(input("enter num1:"))
+num2=int(input("enter num2:"))
+
+print("Addition-",add(num1,num2))
+print("Subtraction-",sub(num1,num2))
+print("Division-",div(num1,num2))
+print("Multipication-",mul(num1,num2))
